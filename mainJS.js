eb3b0b815ba891c15b9518c271d17518cd04203b
Voqda <ludwig.fuchs@protonmail.com>
2024-11-24 20:14:09 +0100

    Voqu

---
+++ mainJS.js
@@ -0,0 +1,4 @@
+window.backendUrl = "https://online-lectures-cs.thi.de/chat/" +
+"91f60642-f8d4-42ec-a26d-84c7eb95dbc3";
+
+window.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNzMyMzYzNTM1fQ._zPv9T05PVRUlkKmxCc3_tMXD5nTzN6EzO8eRDY60SE";
