745cd6c117f0ef3842753ce19da41cbd6d843ec3
Mangonssen <marc.obst@gmx.net>
2024-11-25 11:30:17 +0100

    corrected collection id in mainJS.js

--- mainJS.js
+++ mainJS.js
@@ -1,4 +1,4 @@
 window.backendUrl = "https://online-lectures-cs.thi.de/chat/" +
-"91f60642-f8d4-42ec-a26d-84c7eb95dbc3";
+"54f425ca-c5bd-4a9a-aa30-2b7107f6dbcb";
 
 window.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNzMyMzYzNTM1fQ._zPv9T05PVRUlkKmxCc3_tMXD5nTzN6EzO8eRDY60SE";
