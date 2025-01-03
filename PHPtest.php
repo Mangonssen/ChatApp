762f5e92d2f7129ebb2901f0e0775e884cd3187b
Mangonssen <marc.obst@gmx.net>
2025-01-03 19:44:28 +0100

    chat.php bootstrap rework

--- PHPtest.php
+++ PHPtest.php
@@ -13,3 +13,41 @@ echo $backendService->loadUser($_SESSION['user'])->getFirstname();
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat</title>
+    
+    <link rel="icon" href="logo.ico" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="chatapp-css.css">
+    <link rel="stylesheet" href="https://use.typekit.net/ngh4elb.css">
+</head>
+<body>
+    <a href="#" class ="btn btn-primary" data-bs-toggle="modal" data-bs-target="#removeFriendModal">Remove Friend</a>
+
+    <div class="modal fade" id="removeFriendModal" tabindex="-1">
+        <div class="modal-dialog modal-dialog-dark">
+            <div class="modal-content bg-dark text-white">
+                <div class="modal-header">
+                    <h5 class="modal-title">Remove friend</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Are you sure you want to remove this friend?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <a href="friends.php?action=remove&friend=<?php echo $_GET['friend']?>" class="btn btn-primary">Remove</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
--- chat.php
+++ chat.php
@@ -22,10 +22,13 @@ $chatPartner = htmlspecialchars($_GET['friend']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat</title>
+    
+    <link rel="icon" href="logo.ico" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
     <link rel="stylesheet" href="chatapp-css.css">
     <link rel="stylesheet" href="https://use.typekit.net/ngh4elb.css">
-    <link rel="icon" href="logo.ico" type="image/x-icon">
-    
 </head>
 <body>
 
@@ -47,22 +50,43 @@ $chatPartner = htmlspecialchars($_GET['friend']);
     <!-- Navigationslinks -->
     <nav class="links">
         <a href="friends.php">← Back</a>
-        <a href="<?php printf('profile.php?username=%s', $_GET['friend']) ?>">Profile</a>
-        <a href="<?php printf('friends.php?action=remove&friend=%s', $_GET['friend']) ?>">Remove Friend</a>
+        <a href="<?php printf('profile.php?username=%s', $_GET['friend']) ?>" >Profile</a>
+        <a href="<?php printf('friends.php?action=remove&friend=%s', $_GET['friend']) ?>" data-bs-toggle="modal" data-bs-target="#removeFriendModal">Remove Friend</a>
     </nav>
+
+    <div class="modal fade" id="removeFriendModal" tabindex="-1">
+        <div class="modal-dialog modal-dialog-dark">
+            <div class="modal-content bg-dark text-white">
+                <div class="modal-header">
+                    <h5 class="modal-title">Remove friend</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Are you sure you want to remove this friend?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <a href="friends.php?action=remove&friend=<?php echo $_GET['friend']?>" class="btn btn-primary">Remove</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <div id="chat" class="chat"> <!-- Chat wird per JS erzeugt! -->
     </div> 
 
     <div class="input-bg"></div>
 
-
-    <div class="chat-input">
-            <form id="mInput">
-                <input type="text" name="msg" id="msg" required placeholder="Type a message...">
-                <button type="button" id="sendButton">Send</button>
-            </form>
+    
+    <div class="chat-input-wrapper">
+    <form id="mInput">
+        <div class="input-group mb-3">
+            <input type="text" name="msg" id="msg" class="form-control"  placeholder="Enter message..." aria-label="Enter message..." aria-describedby="button-addon2">
+            <button class="btn btn-outline-secondary" type="button" id="sendButton">Send</button>
+        </div>
+    </form>
     </div>
+    
 
 
     <script src="chat-js.js"></script>
