35fb51f53b5177a273790cd9865ad46738613de9
Mangonssen <marc.obst@gmx.net>
2025-01-05 19:05:49 +0100

    removal of link on logo

--- chat.php
+++ chat.php
@@ -27,7 +27,7 @@ $chatPartner = htmlspecialchars($_GET['friend']);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
-    <link rel="stylesheet" href="chatapp-css.css">
+    <link rel="stylesheet" href="chatapp-css.css?v=<?version?>">
     <link rel="stylesheet" href="https://use.typekit.net/ngh4elb.css">
 </head>
 <body>
@@ -36,14 +36,15 @@ $chatPartner = htmlspecialchars($_GET['friend']);
 
         <!-- Header mit Logo -->
         <header class="site-header">
-            <a href="profile.php0">
+            
                 <img src="logo.png" alt="logo" id="logo" >
-            </a>
+            
         </header>
     
     <!-- Titel -->
     <div id="chat-title" class="heading-container-centered">
-      </div>
+          
+    </div>
 
     <hr>
 
